d5a4d7ce27cd62a86c346264bbe2346078dfc6f7
Kyle Deng <kyle.deng01@gmail.com>
2025-10-02 22:58:57 -0400

    feat: integrate GET /projects Lambda with API Gateway endpoint

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,6 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Table, AttributeType, BillingMode} from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { RestApi, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
 
 export class PersonalSiteBackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,11 +16,23 @@ export class PersonalSiteBackendStack extends cdk.Stack {
       billingMode: BillingMode.PAY_PER_REQUEST,
     });
 
-    // Add a Global Secondary Index (GSI)
+    // Global Secondary Index (GSI)
     table.addGlobalSecondaryIndex({
       indexName: 'GSI1',
       partitionKey: { name: 'GSI1PK', type: AttributeType.STRING },
       sortKey: { name: 'GSISK', type: AttributeType.STRING },
     });
+
+    const projectsGetAll = new NodejsFunction(this, 'ProjectsGetALl', {
+      entry: path.join(__dirname, '..', 'lambda', 'projectsGetAll.ts'),
+      runtime: Runtime.NODEJS_20_X,
+      environment: { Table: table.tableName, GSI1: 'GSI1' }
+    });
+
+    table.grantReadData(projectsGetAll);
+
+    const api = new RestApi(this, 'PersonalSiteApi');
+    api.root.addResource('projects')
+      .addMethod('GET', new LambdaIntegration(projectsGetAll));
   }
 }
