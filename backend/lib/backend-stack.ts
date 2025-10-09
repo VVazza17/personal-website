46a65eb67e3da15fce430fdb308840dd455534e7
Kyle Deng <kyle.deng01@gmail.com>
2025-10-09 19:52:21 -0400

    feat: add S3 bucket + QaCache DDB + env vars

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -5,6 +5,9 @@ import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { RestApi, LambdaIntegration, Cors } from 'aws-cdk-lib/aws-apigateway';
 import * as path from 'path';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as ddb from 'aws-cdk-lib/aws-dynamodb';
+import { RemovalPolicy } from 'aws-cdk-lib';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -21,6 +24,20 @@ export class BackendStack extends cdk.Stack {
       indexName: 'GSI1',
       partitionKey: { name: 'GSI1PK', type: AttributeType.STRING },
       sortKey: { name: 'GSI1SK', type: AttributeType.STRING },
+    });
+
+    // S3 bucket for docs
+    const docsBucket = new s3.Bucket(this, 'DocsBucket', {
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // DynamoDB cache table
+    const cacheTable = new ddb.Table(this, 'QaCache', {
+      partitionKey: { name: 'pk', type: ddb.AttributeType.STRING },
+      billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+      timeToLiveAttribute: 'ttl',
+      removalPolicy: RemovalPolicy.DESTROY,
     });
 
     // GET /projects
@@ -51,9 +68,19 @@ export class BackendStack extends cdk.Stack {
     const chatPost = new NodejsFunction(this, "ChatPost", {
       entry: path.join(__dirname, "..", "lambda", "chatPost.ts"),
       runtime: Runtime.NODEJS_20_X,
-      environment: { TABLE_NAME: table.tableName },
+      environment: { 
+        TABLE_NAME: table.tableName,
+        DDB_TABLE: cacheTable.tableName,
+        S3_BUCKET: docsBucket.bucketName,
+        CACHE_TTL_HOURS: process.env.CACHE_TTL_HOURS ?? '48',
+        USE_RERANK: process.env.PG_CONN ?? '',
+        PG_SCHEMA: process.env.PG_SCHEMA ?? 'public'
+      },
+      memorySize: 512,
     });
     table.grantWriteData(chatPost);
+    cacheTable.grantReadWriteData(chatPost);
+    docsBucket.grantRead(chatPost)
 
     const api = new RestApi(this, 'PersonalSiteApi', {
       defaultCorsPreflightOptions: {
